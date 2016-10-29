5c59efc6bd6d0686a3d1973ab90437ecb6e01211
kantuni <henrikh.kantuni@gmail.com>
2016-10-29 01:56:36 -0400

    Positive Con Sequences

---
+++ 7295/positive_con_sequences.py
@@ -0,0 +1,62 @@
+""" Created by Henrikh Kantuni on 10/29/16. """
+
+
+def is_equal(a, b, ignore):
+    """
+    Determine if a is equal to b ignoring an element with the given index
+    :param a: {list} a
+    :param b: {list} b
+    :param ignore: {int} index of an element to ignore
+    :return: True or False
+    """
+    a, b = a[:], b[:]
+    del a[ignore]
+    del b[ignore]
+
+    if a == b:
+        return True
+    else:
+        return False
+
+
+while True:
+    sequence = input()
+    if sequence == '-1 -1 -1 -1':
+        break
+
+    original = sequence.split()
+    original = [int(s) for s in original]
+    derived = original[:]
+    index_of_unknown = original.index(-1)
+
+    # check arithmetic sequence
+    if index_of_unknown < 2:
+        diff = derived[3] - derived[2]
+        derived[1] = derived[2] - diff
+        derived[0] = derived[1] - diff
+    else:
+        diff = derived[1] - derived[0]
+        derived[2] = derived[1] + diff
+        derived[3] = derived[2] + diff
+
+    if is_equal(original, derived, index_of_unknown):
+        if 1 <= derived[index_of_unknown] <= 1000000:
+            print(derived[index_of_unknown])
+            continue
+
+    # check geometric sequence
+    if index_of_unknown < 2:
+        factor = int(derived[3] / derived[2])
+        derived[1] = int(derived[2] / factor)
+        derived[0] = int(derived[1] / factor)
+    else:
+        factor = int(derived[1] / derived[0])
+        derived[2] = derived[1] * factor
+        derived[3] = derived[2] * factor
+
+    if is_equal(original, derived, index_of_unknown):
+        if 1 <= derived[index_of_unknown] <= 1000000:
+            print(derived[index_of_unknown])
+            continue
+
+    print(-1)
