a61f39f5cdb8ac1f255e99a3a9a99d298e3b4ed9
kantuni <henrikh.kantuni@gmail.com>
2016-10-31 18:23:37 -0400

    Added if __name__ == __main__

--- 7295/positive_con_sequences.py
+++ 7295/positive_con_sequences.py
@@ -15,44 +15,46 @@ def is_equal(a, b, ignore):
     return True if a == b else False
 
 
-while True:
-    sequence = input()
-    if sequence == '-1 -1 -1 -1':
-        break
+if __name__ == '__main__':
+    while True:
+        sequence = input()
+        original = sequence.split()
+        original = [int(s) for s in original]
 
-    original = sequence.split()
-    original = [int(s) for s in original]
-    derived = original[:]
-    index_of_unknown = original.index(-1)
+        if original == [-1, -1, -1, -1]:
+            break
 
-    # arithmetic sequence
-    if index_of_unknown < 2:
-        diff = derived[3] - derived[2]
-        derived[1] = derived[2] - diff
-        derived[0] = derived[1] - diff
-    else:
-        diff = derived[1] - derived[0]
-        derived[2] = derived[1] + diff
-        derived[3] = derived[2] + diff
+        derived = original[:]
+        index_of_unknown = original.index(-1)
 
-    if is_equal(original, derived, index_of_unknown):
-        if 1 <= derived[index_of_unknown] <= 1000000:
-            print(derived[index_of_unknown])
-            continue
+        # arithmetic sequence
+        if index_of_unknown < 2:
+            diff = derived[3] - derived[2]
+            derived[1] = derived[2] - diff
+            derived[0] = derived[1] - diff
+        else:
+            diff = derived[1] - derived[0]
+            derived[2] = derived[1] + diff
+            derived[3] = derived[2] + diff
 
-    # geometric sequence
-    if index_of_unknown < 2:
-        factor = int(derived[3] / derived[2])
-        derived[1] = int(derived[2] / factor)
-        derived[0] = int(derived[1] / factor)
-    else:
-        factor = int(derived[1] / derived[0])
-        derived[2] = derived[1] * factor
-        derived[3] = derived[2] * factor
+        if is_equal(original, derived, index_of_unknown):
+            if 1 <= derived[index_of_unknown] <= 1000000:
+                print(derived[index_of_unknown])
+                continue
 
-    if is_equal(original, derived, index_of_unknown):
-        if 1 <= derived[index_of_unknown] <= 1000000:
-            print(derived[index_of_unknown])
-            continue
+        # geometric sequence
+        if index_of_unknown < 2:
+            factor = int(derived[3] / derived[2])
+            derived[1] = int(derived[2] / factor)
+            derived[0] = int(derived[1] / factor)
+        else:
+            factor = int(derived[1] / derived[0])
+            derived[2] = derived[1] * factor
+            derived[3] = derived[2] * factor
 
-    print(-1)
+        if is_equal(original, derived, index_of_unknown):
+            if 1 <= derived[index_of_unknown] <= 1000000:
+                print(derived[index_of_unknown])
+                continue
+
+        print(-1)
