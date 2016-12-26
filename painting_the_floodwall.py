7d277a72af86839b45fb099d03fe73ff5ba7b2fb
kantuni <henrikh.kantuni@gmail.com>
2016-12-26 14:18:19 +0300

    Painting the Floodwall - Solved

--- painting_the_floodwall.py
+++ painting_the_floodwall.py
@@ -1,7 +1,7 @@
-""" Created by Henrikh Kantuni on 12/25/16 """
+""" Created by Henrikh Kantuni and Shahen Kosyan on 12/25/16 """
 
 
-def get_maximum_total_length():
+def max_total_length(murals):
     """
     DP
 
@@ -9,46 +9,39 @@ def get_maximum_total_length():
     without allowing any two artists' work to overlap.
 
     Recursive formula:
-    Let C[i, j] denote the maximum total length of the fence that can be painted using murals from i to j (inclusive)
-    that don't overlap, then C[i, j] =
+    Let C(A) denote the maximum total length of the fence
+    that can be painted using murals in array A that don't overlap.
+    Then
+    C(A) = max(A[0][1] - A[0][0] + C(B), C(A \ A[0]),
+    where B = array of murals that don't overlap with A[0]
 
+    :param: {int} starting position
+    :param: {int} ending position
     :return: {int} maximum total length of the fence
     """
-    # Example:
-    # 3
-    # 100 200
-    # 190 210
-    # 200 300
-    #
-    # C[0, 2] = the maximum total length of ... using murals from 0 to 2 (inclusive) that don't overlap
-    #
-    # Case 1: (100, 200) is used
-    # then the maximum total length = (200 - 100) + maximum of the rest that don't overlap with (100, 200)
-    #
-    # Case 2: (100, 200) is not used
-    # then the maximum total length = maximum of the rest
-    pass
+    if not murals:
+        return 0
+
+    no_overlap = []
+    for mural in murals:
+        if mural[1] <= murals[0][0] or mural[0] >= murals[0][1]:
+            no_overlap.append(mural)
+
+    value = murals[0][1] - murals[0][0]
+    del murals[0]
+    return max(value + max_total_length(no_overlap), max_total_length(murals))
 
 
 if __name__ == '__main__':
     while True:
-        params = input().split()
-        params = [int(p) for p in params]
-
-        # number of paintings
-        n = params[0]
-
-        # ???
-        if n == 1:
-            print(1)
-            continue
+        n = int(input())
 
         paintings = []
-        C = []  # costs
 
         while n > 0:
             x0, x1 = input().split()
             paintings.append((int(x0), int(x1)))
             n -= 1
 
-        print(get_maximum_total_length())
+        answer = max_total_length(paintings)
+        print(answer)
