30affecd75ca6845f90a7f916297b8015aaf60d4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-02-05 01:36:16 -0500

    Update fifty_coats_of_gray.py

--- 6192/fifty_coats_of_gray.py
+++ 6192/fifty_coats_of_gray.py
@@ -10,12 +10,7 @@ if __name__ == '__main__':
         if params == [0, 0, 0, 0, 0, 0]:
             break
 
-        n = params[0]
-        w = params[1]
-        l = params[2]
-        h = params[3]
-        area = params[4]
-        m = params[5]
+        n, w, l, h, area, m = params
 
         not_painted_area = 0
         while m > 0:
