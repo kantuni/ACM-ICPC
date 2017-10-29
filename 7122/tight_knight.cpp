a0103f4040aa9af921de6018a7bec88178e83f01
kantuni <henrikh.kantuni@gmail.com>
2017-10-29 01:46:02 -0400

    Turns out it's a max flow problem

--- 7122/tight_knight.cpp
+++ 7122/tight_knight.cpp
@@ -15,10 +15,7 @@ int n, m, si, sj, ei, ej, c;
 int dr[] = {-1, 1, -2, 2, -2, 2, -1, 1};
 int dc[] = {-2, -2, -1, -1, 1, 1, 2, 2};
 vvvii AdjList;
-vvii parent;
-vvi color, num, low;
-set<ii> artics;
-int timer;
+vvi color;
 
 
 void print() {
@@ -40,33 +37,11 @@ void populate(int i, int j) {
   for (int k = 0; k < 8; k++) {
     int r = i + dr[k], c = j + dc[k];
     if (r >= 0 && r < n && c >= 0 && c < m) {
-      AdjList[i][j].push_back(ii(r, c));
-      if (color[r][c] == UNVISITED)
-        populate(r, c); 
-    }
-  }
-}
+      if (color[r][c] != OBSTACLE)
+        AdjList[i][j].push_back(ii(r, c));
 
-void articulationPoints(int ui, int uj) {
-  color[ui][uj] = VISITED;
-  low[ui][uj] = num[ui][uj] = timer++;
-  
-  for (int k = 0; k < AdjList[ui][uj].size(); k++) {
-    ii v = AdjList[ui][uj][k];
-    int vi = v.first, vj = v.second;
-    ii p = parent[vi][vj];
-    
-    if (color[vi][vj] == UNVISITED) {
-      parent[vi][vj] = ii(ui, uj);
-      articulationPoints(vi, vj);
-      
-      if (low[vi][vj] >= num[ui][uj]) {
-        artics.insert(ii(ui, uj));
-      }
-      
-      low[ui][uj] = min(low[ui][uj], low[vi][vj]);
-    } else if (p.first != ui && p.second != uj) {
-      low[ui][uj] = min(low[ui][uj], num[vi][vj]);
+      if (color[r][c] == UNVISITED)
+        populate(r, c);
     }
   }
 }
@@ -89,33 +64,6 @@ int main() {
     // create graph
     populate(si, sj);
     print();
-    
-    timer = 0;
-    parent.assign(n, vii(m));
-    parent[si][sj] = ii(-1, -1);
-    color.assign(n, vi(m, UNVISITED));
-    num.assign(n, vi(m, 0));
-    low.assign(n, vi(m, 0));
-    
-    // find articulation points
-    articulationPoints(si, sj);
-    
-    int counter = 0;
-    for (auto point : artics) {
-      cout << point.first << " " << point.second << "\n";
-      bool start = point.first == si && point.second == sj;
-      bool end = point.first == ei && point.second == ej;
-      if (!start && !end)
-        counter++;
-    }
-    
-    if (counter >= 1) {
-      cout << "Yes\n";
-    } else {
-      cout << "No\n";
-    }
-    
-    artics.clear();
   }
   
   return 0;
