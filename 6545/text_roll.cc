55cce662cc6674495c5e90c6e2b0057dd94e5230
kantuni <henrikh.kantuni@gmail.com>
2017-10-31 06:03:17 -0400

    6545 - Accepted

---
+++ 6545/text_roll.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    cin.ignore();
+    
+    if (n == 0)
+      break;
+    
+    vector<string> lines;
+    for (int i = 0; i < n; i++) {
+      string line;
+      getline(cin, line);
+      lines.push_back(line);
+    }
+    
+    int start = 0;
+    for (int i = 0; i < lines.size(); i++) {
+      bool found = false;
+      for (int j = start; j < lines[i].size(); j++) {
+        if (lines[i][j] == ' ') {
+          found = true;
+          start = j;
+          break;
+        }
+      }
+      if (!found) start = max(start, (int) lines[i].size());
+    }
+    
+    // answer
+    cout << start + 1 << "\n";
+  }
+  
+  return 0;
+}
