27f2cbedac8f3f721d8dae6861b92d14230a1bad
kantuni <henrikh.kantuni@gmail.com>
2017-11-09 16:25:16 -0500

    3579 - AC

--- 3579/permutation_recovery.cc
+++ 3579/permutation_recovery.cc
@@ -18,14 +18,50 @@ int main() {
       cin >> a[i];
     }
     
-    /*
+    for (int i = 0; i < n; i++) {
+      int count = 0;
+      auto ith = find(p.begin(), p.end(), i + 1);
+      for (auto it = p.begin(); it != ith; it++) {
+        if (*it > *ith)
+          count++;
+      }
+      
+      if (count == a[i])
+        continue;
+        
+      if (count > a[i]) {
+        int left = count - a[i];
+        for (auto it = prev(ith); it != prev(p.begin()); it--) {
+          if (*it > *ith)
+            left--;
+            
+          if (left == 0) {
+            swap(*it, *ith);
+            break;
+          }
+        }
+      }
+      
+      if (count < a[i]) {
+        int left = a[i] - count;
+        for (auto it = next(ith); it != p.end(); it++) {
+          if (*it > *ith)
+            left--;
+            
+          if (left == 0) {
+            swap(*it, *ith);
+            break;
+          }
+        }
+      }
+    }
+    
     // print answer
     for (int i = 0; i < n; i++) {
       if (i != 0) cout << ",";
       cout << p[i];
     }
     cout << "\n";
-    */
   }
   
   return 0;
