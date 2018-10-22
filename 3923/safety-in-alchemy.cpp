690927677d36ffe44cf471fe77bfdbfe4d5baf12
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-10-22 00:13:50 -0400

    Update safety-in-alchemy.cpp

--- 3923/safety-in-alchemy.cpp
+++ 3923/safety-in-alchemy.cpp
@@ -1,4 +1,3 @@
-// To be continued...
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,7 +5,7 @@ unordered_map<string, int> rs;
 unordered_map<string, int> cs;
 
 int solve() {
-  
+  // TODO:
 }
 
 int main() {
@@ -14,17 +13,15 @@ int main() {
   while (getline(cin, line)) {
     if (line == "") continue;
     if (line == "ENDOFINPUT") break;
-    
     istringstream iss(line);
     string c1, c2; int h;
     iss >> c1 >> c2 >> h;
-    
-    if (c1 == "0" && c2 == "0" && h == 0) {
+    if (c1 == "0" and c2 == "0" and h == 0) {
       while (true) {
-        string c; int a;
+        string c;
+        int a;
         cin >> c >> a;
-        
-        if (c == "0" && a == 0) {
+        if (c == "0" and a == 0) {
           int ans = solve();
           printf("The temperature in the jar will change by at most %d degrees.\n", ans);
           rs.clear();
@@ -37,6 +34,5 @@ int main() {
     }
     rs[c1 + " + " + c2] = h;
   }
-  
   return 0;
 }
