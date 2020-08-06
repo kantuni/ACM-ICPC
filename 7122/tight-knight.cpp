b1ba33e708a473b02191ac8927ed2737e912d8e7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-08-06 15:33:56 -0400

    Update tight-knight.cpp

--- 7122/tight-knight.cpp
+++ 7122/tight-knight.cpp
@@ -25,19 +25,25 @@ void print() {
         ii v = AdjList[i][j][k];
         cout << "(" << v.first << ", " << v.second << ") ";
       }
-      cout << "\n";
+      cout << endl;
     }
   }
 }
 
 void populate(int i, int j) {
   color[i][j] = VISITED;
-  if (i == ei && j == ej) return;
+  if (i == ei && j == ej) {
+    return;
+  }
   for (int k = 0; k < 8; k++) {
     int r = i + dr[k], c = j + dc[k];
     if (r >= 0 && r < n && c >= 0 && c < m) {
-      if (color[r][c] != OBSTACLE) AdjList[i][j].push_back(ii(r, c));
-      if (color[r][c] == UNVISITED) populate(r, c);
+      if (color[r][c] != OBSTACLE) {
+        AdjList[i][j].push_back(ii(r, c));
+      }
+      if (color[r][c] == UNVISITED) {
+        populate(r, c);
+      }
     }
   }
 }
@@ -46,21 +52,19 @@ int main() {
   while (true) {
     cin >> n >> m >> si >> sj >> ei >> ej >> c;
     si--; sj--; ei--; ej--;
-    if (n == 0) break;
-    
+    if (n == 0) {
+      break;
+    }
     AdjList.assign(n, vvii(m));
     color.assign(n, vi(m, UNVISITED));
-    
     while (c--) {
       int x, y;
       cin >> x >> y;
       color[x - 1][y - 1] = OBSTACLE;
     }
-    
     // create graph
     populate(si, sj);
     print();
   }
-  
   return 0;
 }
