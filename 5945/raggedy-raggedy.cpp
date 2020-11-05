2d34271999bab72bd82b479220aaf1fb91e52ae5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-11-04 21:34:33 -0500

    Update raggedy-raggedy.cpp

--- 5945/raggedy-raggedy.cpp
+++ 5945/raggedy-raggedy.cpp
@@ -30,14 +30,15 @@ int w(int i, int j) {
 }
 
 int r(int i, int j) {
-  if (w(i, j) > L) return -1;
+  if (w(i, j) > L) {
+    return -1;
+  }
   return (L - w(i, j)) * (L - w(i, j));
 }
 
 void populate(int i, int j) {
   color[i][j] = VISITED;
   bool found = false;
-
   for (int k = j + 1; k <= p.size(); k++) {
     int w = r(j + 1, k);
     if (w != -1) {
@@ -48,14 +49,13 @@ void populate(int i, int j) {
       found = true;
       ii v = ii(j + 1, k);
       AdjList[i][j].push_back(iii(w, v));
-      if (color[j + 1][k] == UNVISITED)
+      if (color[j + 1][k] == UNVISITED) {
         populate(j + 1, k);
+      }
     }
   }
-  
   // topological sort
   ts.push_back(ii(i, j));
-  
   if (!found) {
     ii v = ii(p.size() + 1, p.size() + 1);
     AdjList[i][j].push_back(iii(0, v));
@@ -65,7 +65,6 @@ void populate(int i, int j) {
 void sssp() {
   reverse(ts.begin(), ts.end());
   ts.push_back(ii(p.size() + 1, p.size() + 1));
-  
   for (int k = 0; k < ts.size(); k++) {
     int ui = ts[k].first, uj = ts[k].second;
     for (int l = 0; l < AdjList[ui][uj].size(); l++) {
@@ -82,8 +81,9 @@ void sssp() {
 }
 
 void recover(int i, int j) {
-  if (i == -1 && j == -1) return;
-  
+  if (i == -1 && j == -1) {
+    return;
+  }
   if (i - 1 >= 0 && j - 1 < p.size()) {
     string s = "";
     for (int k = i - 1; k <= j - 1; k++) {
@@ -92,7 +92,6 @@ void recover(int i, int j) {
     }
     ans.push_back(s);
   }
-  
   ii v = parent[i][j];
   recover(v.first, v.second);
 }
@@ -100,15 +99,19 @@ void recover(int i, int j) {
 int main() {
   while (true) {
     cin >> L; cin.ignore();
-    if (L == 0) break;
-    
+    if (L == 0) {
+      break;
+    }
     string line, word;
     while (getline(cin, line)) {
-      if (line.size() == 0) break;
+      if (line.size() == 0) {
+        break;
+      }
       istringstream iss(line);
-      while (iss >> word) p.push_back(word);
+      while (iss >> word) {
+        p.push_back(word);
+      }
     }
-    
     int N = p.size() + 2;
     AdjList.assign(N, vviii(N));
     color.assign(N, vi(N, UNVISITED));
@@ -116,34 +119,27 @@ int main() {
     dist[0][0] = 0;
     parent.assign(N, vii(N, ii(INF, INF)));
     parent[0][0] = ii(-1, -1);
-    
     last = 0;
     for (int i = p.size(); i > 0; i--) {
       if (w(i, p.size()) <= L) {
         last = p.size() - i + 1;
       }
     }
-    
     // create DAG
     populate(0, 0);
-    
     // find the shortest path
     sssp();
-    
     // recover the path
     recover(N - 1, N - 1);
-    
     // print the path
-    for (int i = ans.size() - 1; i >= 0; i--) {
-      cout << ans[i] << "\n";
+    for (int i = ans.size() - 1; i > -1; i--) {
+      cout << ans[i] << endl;
     }
-    cout << "===" << "\n";
-    
+    cout << "===" << endl;
     // reset
     p.clear();
     ans.clear();
     ts.clear();
   }
-  
   return 0;
 }
