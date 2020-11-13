48847bb7c6b26985da4d4589675431f85c3cacb2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-11-13 15:06:53 -0500

    Update talking-about-numbers.py

--- 7348/talking-about-numbers.py
+++ 7348/talking-about-numbers.py
@@ -1,14 +1,14 @@
 """ Created by Henrikh Kantuni on 11/2/16 """
 
 
-zero_to_nineteen = ['zero', 'one', 'two', 'three', 'four', 'five',
-                    'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
-                    'twelve', 'thirteen', 'fourteen', 'fifteen',
-                    'sixteen', 'seventeen', 'eighteen', 'nineteen']
+zero_to_nineteen = ["zero", "one", "two", "three", "four", "five",
+                    "six", "seven", "eight", "nine", "ten", "eleven",
+                    "twelve", "thirteen", "fourteen", "fifteen",
+                    "sixteen", "seventeen", "eighteen", "nineteen"]
 
-multiples_of_10 = ['you don\'t need this', 'ten',
-                   'twenty', 'thirty', 'forty', 'fifty',
-                   'sixty', 'seventy', 'eighty', 'ninety']
+multiples_of_10 = ["you don't need this", "ten",
+                   "twenty", "thirty", "forty", "fifty",
+                   "sixty", "seventy", "eighty", "ninety"]
 
 
 def from_0_to_99(n):
@@ -28,7 +28,7 @@ def from_0_to_99(n):
         # tens
         if n % 10 == 0:
             return multiples_of_10[n // 10]
-        return multiples_of_10[n // 10] + '-' + zero_to_nineteen[n % 10]
+        return multiples_of_10[n // 10] + "-" + zero_to_nineteen[n % 10]
 
 
 def from_100_to_999(n):
@@ -38,13 +38,13 @@ def from_100_to_999(n):
     :param n: {int} number
     :return: {str} spelled-out equivalent of the number
     """
-    answer = ''
+    answer = ""
 
     # hundreds
-    answer += zero_to_nineteen[n // 100] + ' hundred'
+    answer += zero_to_nineteen[n // 100] + " hundred"
     # tens
     if n % 100 > 0:
-        answer += ' and ' + from_0_to_99(n % 100)
+        answer += " and " + from_0_to_99(n % 100)
 
     return answer
 
@@ -56,21 +56,21 @@ def from_1000_to_999999(n):
     :param n: {int} number
     :return: {str} spelled-out equivalent of the number
     """
-    answer = ''
+    answer = ""
 
     # tens of thousands
     if 0 <= n // 1000 <= 99:
-        answer += from_0_to_99(n // 1000) + ' thousand'
+        answer += from_0_to_99(n // 1000) + " thousand"
     # hundreds of thousands
     else:
-        answer += from_100_to_999(n // 1000) + ' thousand'
+        answer += from_100_to_999(n // 1000) + " thousand"
 
     # tens
     if 0 < n % 1000 <= 99:
-        answer += ' and ' + from_0_to_99(n % 1000)
+        answer += " and " + from_0_to_99(n % 1000)
     # hundreds
     elif 100 <= n % 1000 <= 999:
-        answer += ', ' + from_100_to_999(n % 1000)
+        answer += ", " + from_100_to_999(n % 1000)
 
     return answer
 
@@ -82,24 +82,24 @@ def from_1000000_to_999999999(n):
     :param n: {int} number
     :return: {str} spelled-out equivalent of the number
     """
-    answer = ''
+    answer = ""
 
     # tens of millions
     if 0 < n // 1000000 <= 99:
-        answer += from_0_to_99(n // 1000000) + ' million'
+        answer += from_0_to_99(n // 1000000) + " million"
     # hundreds of millions
     else:
-        answer += from_100_to_999(n // 1000000) + ' million'
+        answer += from_100_to_999(n // 1000000) + " million"
 
     # tens
     if 0 < n % 1000000 <= 99:
-        answer += ' and ' + from_0_to_99(n % 1000000)
+        answer += " and " + from_0_to_99(n % 1000000)
     # hundreds
     elif 100 <= n % 1000000 <= 999:
-        answer += ', ' + from_100_to_999(n % 1000000)
+        answer += ", " + from_100_to_999(n % 1000000)
     # thousands
     elif 1000 <= n % 1000000 <= 999999:
-        answer += ', ' + from_1000_to_999999(n % 1000000)
+        answer += ", " + from_1000_to_999999(n % 1000000)
 
     return answer
 
@@ -121,7 +121,7 @@ def spell_number(n):
         return from_1000000_to_999999999(n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
         number = int(input())
 
