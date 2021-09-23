2244f1fe9cde7083c8cf43f180039bb984992534
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-09-23 13:15:23 -0400

    Update periodic-strings.cpp

--- 7948/periodic-strings.cpp
+++ 7948/periodic-strings.cpp
@@ -1,57 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool periodic(string &s, int k) {
+  for (int i = k; i < s.size(); i += k) {
+    string prv = s.substr(i - k, k);
+    string tmp = prv.back() + prv.substr(0, prv.size() - 1);
+    string cur = s.substr(i, k);
+    if (tmp != cur) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   string s;
-  while (getline(cin, s)) {
-    if (s.size() == 0) {
-      continue;
-    }
-    // a
-    if (s.size() == 1) {
-      cout << 1 << endl;
-      continue;
-    }
-    set<char> sc;
-    for (char c: s) {
-      sc.insert(c);
-    }
-    // aaa
-    if (sc.size() == 1) {
-      cout << 1 << endl;
-      continue;
-    }
-    if (sc.size() == s.size()) {
-      cout << s.size() << endl;
-      continue;
-    }
-    // abba, abccab, ...
-    for (int k = 2; k <= s.size(); k++) {
-      if (s.size() % k == 0) {
-        vector<string> subs;
-        string tmp;
-        for (int i = 0; i < s.size(); i++) {
-          if (i > 0 && i % k == 0) {
-            subs.push_back(tmp);
-            tmp = "";
-          }
-          tmp += s[i];
-        }
-        subs.push_back(tmp);
-        bool periodic = true;
-        for (int i = 1; i < subs.size(); i++) {
-          string cur = subs[i - 1];
-          cur = cur[cur.size() - 1] + cur;
-          cur = cur.substr(0, cur.size() - 1);
-          if (subs[i] != cur) {
-            periodic = false;
-            break;
-          }
-        }
-        if (periodic) {
-          cout << k << endl;
-          break;
-        }
+  while (cin >> s) {
+    for (int k = 1; k <= s.size(); k++) {
+      if (periodic(s, k)) {
+        cout << k << endl;
+        break;
       }
     }
   }
